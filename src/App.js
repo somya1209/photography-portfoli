257e4e2374327a9fa925596ceea5c56ae4062d08
somya1209 <somya123024@gmail.com>
2021-05-28 20:11:25 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+function randomFill(word){
+  var newWord="";
+ for (let index = 0; index < word.length; index++) {
+   document.write("<span></span")
+   var rand = Math.floor((Math.random()*100)+1);
+   if (rand%5==0)  {newWord += word.charAt(index).toLowerCase(); }
+   else newWord += word.charAt(index);
+   
+ }
+return newWord;
 
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header id="1" className="App-header">
+       <div className="theme-circle">
+         <img className="center-photo" src="https://images.pexels.com/photos/5878801/pexels-photo-5878801.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"></img>
+       </div>
+       <div className="titles">
+      <h1 >{randomFill( "BETTER ONE")}</h1>
+      <h1 id="h">{randomFill( "FREE YOURSELF")}</h1>
+      <h1 id="h">{randomFill( "TOGETHER")}</h1>
+      <h1 id="h">{randomFill( "EXPANSION")}</h1>
+      </div>
       </header>
+      <header id="sec" className="App-header" style={{backgroundColor:"black"}}></header>
     </div>
   );
 }
